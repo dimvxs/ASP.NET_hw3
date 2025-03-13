2b6f726a9d92b690bd9c535e0904207c5cdd956f
dimathecreator <dzhosan777@gmail.com>
2025-03-13 16:18:53 +0100

    new

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Principal;
 using hw.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -6,17 +8,113 @@ namespace hw.Controllers
 {
     public class MoviesController: Controller
     {
-        MovieContext db;
-        public MoviesController(MovieContext context)
+       private readonly MovieContext context;
+        public MoviesController(MovieContext _context)
         {
-            db = context;
+            context = _context;
         }
 
         public async Task<IActionResult> GetMovies()
         {
-            IEnumerable<Movie> movies = await db.Movies.ToListAsync(); 
-            ViewBag.Movies = movies;
+            // IEnumerable<Movie> movies = await context.Movies.ToListAsync(); 
+            // ViewBag.Movies = movies;
+            // return View();
+                 return View(await context.Movies.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id){
+            if(id == null){
+                return NotFound();
+            }
+            var movie = await context.Movies.FirstOrDefaultAsync(m => m.Id == id);
+            if(movie == null){
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        public IActionResult Create(){
             return View();
         }
+
+[HttpPost]
+[ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id, Name, Director, Genre, Year, Poster, Description")] Movie movie)
+        {
+            if(ModelState.IsValid)
+            {
+                context.Add(movie);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(GetMovies));
+            }
+            return View(movie);
+        }
+
+
+        public async Task<IActionResult> Edit(int? id){
+            if(id == null){
+                return NotFound();
+            }
+            var movie = await context.Movies.FindAsync(id);
+            if(movie == null){
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Director, Genre, Year, Poster, Description")] Movie movie) {
+if(ModelState.IsValid){
+    try{
+    context.Update(movie);
+    await context.SaveChangesAsync();
+    }
+    catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(GetMovies));
+}
+      return View(movie);
+
+        }
+
+
+        private async Task<IActionResult> Delete(int? id){
+            if(id == null){
+                return NotFound();
+            }
+            var movie = await context.Movies.FirstOrDefaultAsync(m => m.Id == id);
+            if(movie == null){
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id){
+           var movie = await context.Movies.FindAsync(id);
+           if(movie != null){
+            context.Movies.Remove(movie);
+           }
+           await context.SaveChangesAsync();
+           return RedirectToAction(nameof(GetMovies));
+        }
+
+        private bool MovieExists(int id){
+            return context.Movies.Any(m => m.Id == id);
+        }
+      
+
     }
 }
